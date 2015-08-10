fdf755b16cc7b4fac5c01760054ec1c599a8dfcc
ghbhaha <ghbhaha@gmail.com>
2015-08-10 13:14:20 +0800

    [1/2]frameworks/base: Add three-fingers-swipe to screenshot
    The feature is ported from Oppo ColorOS.
    With the options on, users can swipe down
    on the screen to have a screenshot.
    
    -Sources: SudaMod
     https://github.com/SudaMod
    
    -Original smali codes: wuxianlin
    
    Change-Id: I6b676ac16e4748cd6781cd4f3f146f7c02a77db4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3681,6 +3681,13 @@ public final class Settings {
         public static final String LONG_PRESS_KILL_DELAY = "long_press_kill_delay";
 
         /**
+         * Three Finger Gesture from Oppo
+         *
+         * @hide
+         */
+        public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
+
+        /**
          * This preference enables showing the power menu on LockScreen.
          * @hide
          */
@@ -3776,10 +3783,10 @@ public final class Settings {
          */
         public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
 
-        /**  
+        /**
          * AOKP Custom System Animations
          * @hide
-         */  
+         */
         public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
                 "activity_open",
                 "activity_close",
@@ -3792,7 +3799,7 @@ public final class Settings {
                 "wallpaper_intra_open",
                 "wallpaper_intra_close",
                 "task_open_behind",
-        };  
+        };
         public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
 
         /**
@@ -3976,7 +3983,7 @@ public final class Settings {
          * @hide
          */
         public static final String LOCK_SCREEN_SHOW_WEATHER_LOCATION = "lock_screen_show_weather_location";
-		
+
         /**
          * Whether to show the weather update timestamp on the lock screen
          * @hide
